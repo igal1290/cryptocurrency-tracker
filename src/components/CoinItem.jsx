0c810f0cca29cb2ba38932a5164c15bf1b64d929
Qi Da Chen (Mike) <83503846+AnbuCap@users.noreply.github.com>
2022-05-04 00:40:50 -0400

    Made some changes to teh css. and line up the info for crypto

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -3,14 +3,15 @@ import './Coins.css';
 
 const CoinItem = (props) => {
   return (
-    <div className="coin__row">
-      <div className="coin__symbol">
-        <img src={props.coin.image} alt="coin img"></img>
+    <div className = "coin__row">
+      <div className = "coin__symbol">
+        {/* crypto img and info*/}
+        <img src = {props.coin.image} alt="coin img"></img>
         <p>{props.coin.symbol.toUpperCase()}</p>
+        <p>{props.coin.current_price.toLocaleString()}</p>
+        <p>{props.coin.market_cap}</p>
+        <p>{props.coin.price_change_percentage_24h}</p>
       </div>
-      <p>{props.coin.current_price.toLocaleString()}</p>
-      <p>{props.coin.market_cap}</p>
-      <p>{props.coin.price_change_percentage_24h}</p>
     </div>
   );
 };
