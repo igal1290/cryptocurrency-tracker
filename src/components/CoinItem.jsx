58ee7510d72b3a938f9c57aa2df94acb6ddec43f
igal1290 <igalofficial@gmail.com>
2022-05-12 02:57:53 -0400

    added css to Navbar and Coins component and aligned everything properly to the center

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -4,13 +4,25 @@ import './Coins.css';
 const CoinItem = (props) => {
   return (
     <div className="coin__row">
-      <div className="coin__symbol">
-        <img src={props.coin.image} alt="coin img"></img>
-        <p>{props.coin.symbol.toUpperCase()}</p>
+      <div className="coin__display">
+        <img
+          className="coin__image"
+          src={props.coin.image}
+          alt="coin img"
+        ></img>
+        <p className="coin__symbol">{props.coin.symbol.toUpperCase()}</p>
       </div>
-      <p>{props.coin.current_price.toLocaleString()}</p>
-      <p>{props.coin.market_cap}</p>
-      <p>{props.coin.price_change_percentage_24h}</p>
+      <div className="coin__data">
+        <p className="coin__price">
+          {`$${props.coin.current_price.toLocaleString()}`}
+        </p>
+        <p className="coin__marketcap">
+          {`$${props.coin.market_cap.toLocaleString()}`}
+        </p>
+        <p className="coin__percent">
+          {props.coin.price_change_percentage_24h}
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,7 @@
+// images
+import CryptoIcon from '../images/crypto-icon.png';
+// react-router-dom
+import { Link } from 'react-router-dom';
 // css
 import './Navbar.css';
 
@@ -5,7 +9,17 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-        <h1 className="navbar__logo">Noden</h1>
+        <Link to="/">
+          <img
+            className="navbar__logo"
+            src={CryptoIcon}
+            alt="Crypto Icon"
+            width="40"
+            height="40"
+          />
+        </Link>
+
+        <h1 className="navbar__header">Noden</h1>
       </nav>
     </div>
   );
